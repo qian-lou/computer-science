570a69a3bf7f84dab1d2f47b079d597b115f8763
liaozongjian <1327639463@qq.com>
2021-09-21 17:41:20 +0800

    LeetCode-两数之和

---
+++ 数据结构和算法设计/leetcode/src/main/java/com/leetcode/problem0001/Solution.java
@@ -0,0 +1,34 @@
+package com.leetcode.problem0001;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    /**
+     *
+     * 
+     *
+     *
+     *
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> index = new HashMap<>();
+        int[] result = new int[2];
+        int len = nums.length;
+        Integer temp;
+        for (int i = 0; i < len; i++) {
+            temp = index.get(target - nums[i]);
+            if ( temp != null) {
+                result[0] = i;
+                result[1] = temp;
+                return result;
+            }
+            index.put(target - nums[i], i);
+        }
+        return result;
+    }
+}
