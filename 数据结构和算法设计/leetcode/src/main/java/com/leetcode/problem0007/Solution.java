0d373aa4b43e723cc8b8617cb14519a0e8f215f0
liaozj <liaozj@eastcom-sw.com>
2021-09-22 15:44:13 +0800

    LeetCode-整数反转

---
+++ 数据结构和算法设计/leetcode/src/main/java/com/leetcode/problem0007/Solution.java
@@ -0,0 +1,12 @@
+package com.leetcode.problem0007;
+
+class Solution {
+    public int reverse(int x) {
+        long sum = 0;
+        while (x != 0) {
+            sum = sum * 10 + (x % 10);
+            x /= 10;
+        }
+        return sum > Integer.MAX_VALUE || sum < Integer.MIN_VALUE ? 0 : (int)sum;
+    }
+}
