d66d212e3dd4fb55c46d05deccf602ed206e3146
liaozongjian <1327639463@qq.com>
2021-10-02 23:40:21 +0800

    LeetCode-罗马数字转整数

---
+++ 数据结构和算法设计/leetcode/src/main/java/com/leetcode/problem0013/Solution.java
@@ -0,0 +1,31 @@
+package com.leetcode.problem0013;
+
+class Solution {
+    public int romanToInt(String s) {
+        int sum = 0, i = 0, a = 0, b = 0, len = s.length();
+        while (i < len) {
+            a = number(s.charAt(i));
+            if (i == len - 1) {
+                sum += a;
+            } else {
+                b = number(s.charAt(i + 1));
+                sum += a >= b ? a : -a;
+            }
+            i++;
+        }
+        return sum;
+    }
+
+    public int number(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+}
