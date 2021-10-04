cb7e22946c5420c7f89e7554a29b9da5e84e082d
liaozongjian <1327639463@qq.com>
2021-10-04 21:53:36 +0800

    leetcode-0189-旋转数组

---
+++ 数据结构和算法设计/leetcode/problem0189_旋转数组/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        k %= len;
+        rotate(nums, 0, nums.length - 1);
+        rotate(nums, 0, k - 1);
+        rotate(nums, k, nums.length - 1);
+    }
+
+    private void rotate(int[] nums, int start, int end) {
+        while (start < end) {
+            nums[start] = nums[start] ^ nums[end];
+            nums[end] = nums[start] ^ nums[end];
+            nums[start] = nums[start] ^ nums[end];
+            start++;
+            end--;
+        }
+    }
+}
