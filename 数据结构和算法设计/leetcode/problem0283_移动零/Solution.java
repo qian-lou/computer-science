6931af470d611a2325553b57cb7ca10d62aad8fc
liaozongjian <1327639463@qq.com>
2021-10-12 06:11:26 +0800

    leetcode-0283-移动零

---
+++ 数据结构和算法设计/leetcode/problem0283_移动零/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                if (i != j) nums[i] = 0;
+                j++;
+            }
+        }
+    }
+}
